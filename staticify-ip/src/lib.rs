369e13f31a4175daf35e6d924c554d72f79b0078
Tom Hotston <hotston.tom@gmail.com>
2024-12-17 01:13:17 +0000

    test: added tests for most functionality of the software

--- staticify-ip/src/lib.rs
+++ staticify-ip/src/lib.rs
@@ -1,53 +1,91 @@
+use anyhow::Result;
 use mockall::predicate::*;
 use mockall::*;
 use std::net::IpAddr;
 
 #[automock]
-pub trait IpGetter {
-    fn get_public_ip(&self) -> Option<IpAddr>;
-    fn get_last_ip(&self) -> Option<IpAddr>;
+trait IpReconfigurer {
+    fn get_public_ip(&self) -> Result<IpAddr>;
+    fn get_last_ip(&self) -> Result<IpAddr>;
+    fn reconfigure_public_ip(&self);
+    fn readback_configured_ip(&self) -> Result<IpAddr>;
+}
+
+fn compare_ips<A>(ip_getter: A) -> Result<()>
+where
+    A: IpReconfigurer,
+{
+    let public_ip = ip_getter.get_public_ip()?;
+    let last_ip = ip_getter.get_last_ip()?;
+    if public_ip != last_ip {
+        ip_getter.reconfigure_public_ip();
+        if ip_getter.readback_configured_ip()? != public_ip {
+            panic!("IP addresses do not match after reconfiguration");
+        }
+    }
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::net::Ipv4Addr;
 
     #[test]
-    fn get_public_ip_handles_vaild_ip_addresses() {
-        let mut mock = MockIpGetter::new();
+    fn compare_ips_handles_vaild_ip_addresses_that_match() {
+        let mut mock = MockIpReconfigurer::new();
         mock.expect_get_public_ip()
-            .returning(|| "127.0.0.1".parse::<IpAddr>().ok());
-        let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-
-        assert_eq!(mock.get_public_ip().unwrap(), localhost_v4);
+            .returning(|| Ok("127.0.0.1".parse::<IpAddr>()?));
+        mock.expect_get_last_ip()
+            .returning(|| Ok("127.0.0.1".parse::<IpAddr>()?));
+        compare_ips(mock).unwrap();
     }
 
     #[test]
     #[should_panic]
-    fn get_public_ip_handles_invalid_ip_addresses() {
-        let mut mock = MockIpGetter::new();
+    fn compare_ips_handles_invalid_public_ip_address() {
+        let mut mock = MockIpReconfigurer::new();
         mock.expect_get_public_ip()
-            .returning(|| "".parse::<IpAddr>().ok());
-        mock.get_public_ip().unwrap();
-    }
-
-    #[test]
-    fn get_last_ip_handles_vaild_ip_addresses() {
-        let mut mock = MockIpGetter::new();
+            .returning(|| Ok("".parse::<IpAddr>()?));
         mock.expect_get_last_ip()
-            .returning(|| "127.0.0.1".parse::<IpAddr>().ok());
-        let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-
-        assert_eq!(mock.get_last_ip().unwrap(), localhost_v4);
+            .returning(|| Ok("127.0.0.1".parse::<IpAddr>()?));
+        compare_ips(mock).unwrap();
     }
 
     #[test]
     #[should_panic]
-    fn get_last_ip_handles_invalid_ip_addresses() {
-        let mut mock = MockIpGetter::new();
+    fn compare_ips_handles_invalid_last_ip_address() {
+        let mut mock = MockIpReconfigurer::new();
         mock.expect_get_last_ip()
-            .returning(|| "".parse::<IpAddr>().ok());
-        mock.get_last_ip().unwrap();
+            .returning(|| Ok("".parse::<IpAddr>()?));
+        mock.expect_get_public_ip()
+            .returning(|| Ok("127.0.0.1".parse::<IpAddr>()?));
+        compare_ips(mock).unwrap();
+    }
+
+    #[test]
+    fn compare_ips_handles_valid_ip_addresses_that_do_not_match_but_reconfigure_correctly() {
+        let mut mock = MockIpReconfigurer::new();
+        mock.expect_get_public_ip()
+            .returning(|| Ok("127.0.0.2".parse::<IpAddr>()?));
+        mock.expect_get_last_ip()
+            .returning(|| Ok("127.0.0.1".parse::<IpAddr>()?));
+        mock.expect_reconfigure_public_ip().return_const(());
+        mock.expect_readback_configured_ip()
+            .returning(|| Ok("127.0.0.2".parse::<IpAddr>()?));
+        compare_ips(mock).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn compare_ips_handles_valid_ip_addresses_that_do_not_match_and_reconfigure_incorrectly() {
+        let mut mock = MockIpReconfigurer::new();
+        mock.expect_get_public_ip()
+            .returning(|| Ok("127.0.0.2".parse::<IpAddr>()?));
+        mock.expect_get_last_ip()
+            .returning(|| Ok("127.0.0.1".parse::<IpAddr>()?));
+        mock.expect_reconfigure_public_ip().return_const(());
+        mock.expect_readback_configured_ip()
+            .returning(|| Ok("127.0.0.1".parse::<IpAddr>()?));
+        compare_ips(mock).unwrap();
     }
 }
