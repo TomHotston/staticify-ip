4dae69740a1c51e7c8752d52e610d350eebdfe53
Tom Hotston <hotston.tom@gmail.com>
2024-12-17 00:01:10 +0000

    test(IpGetter): add test on type checking for getter functions

--- staticify-ip/src/lib.rs
+++ staticify-ip/src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use mockall::predicate::*;
+use mockall::*;
+use std::net::IpAddr;
+
+#[automock]
+pub trait IpGetter {
+    fn get_public_ip(&self) -> Option<IpAddr>;
+    fn get_last_ip(&self) -> Option<IpAddr>;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::net::Ipv4Addr;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn get_public_ip_handles_vaild_ip_addresses() {
+        let mut mock = MockIpGetter::new();
+        mock.expect_get_public_ip()
+            .returning(|| "127.0.0.1".parse::<IpAddr>().ok());
+        let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+
+        assert_eq!(mock.get_public_ip().unwrap(), localhost_v4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn get_public_ip_handles_invalid_ip_addresses() {
+        let mut mock = MockIpGetter::new();
+        mock.expect_get_public_ip()
+            .returning(|| "".parse::<IpAddr>().ok());
+        mock.get_public_ip().unwrap();
+    }
+
+    #[test]
+    fn get_last_ip_handles_vaild_ip_addresses() {
+        let mut mock = MockIpGetter::new();
+        mock.expect_get_last_ip()
+            .returning(|| "127.0.0.1".parse::<IpAddr>().ok());
+        let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+
+        assert_eq!(mock.get_last_ip().unwrap(), localhost_v4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn get_last_ip_handles_invalid_ip_addresses() {
+        let mut mock = MockIpGetter::new();
+        mock.expect_get_last_ip()
+            .returning(|| "".parse::<IpAddr>().ok());
+        mock.get_last_ip().unwrap();
     }
 }
