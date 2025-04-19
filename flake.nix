654520a8273e9d58e5024111be3823fb59cf80e0
Tom Hotston <hotston.tom@gmail.com>
2025-04-19 19:24:27 +0100

    build(flake): add cargo2nix build system for package

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Rust dev flake";
+  description = "staticify-ip - A tool for reconfiguring cloudflare servers";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,26 +10,40 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
+    cargo2nix = {
+      url = "github:cargo2nix/cargo2nix/release-0.11.0";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }: 
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix }: 
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          overlays = [ (import rust-overlay) ];
+          overlays = [ (import rust-overlay) cargo2nix.overlays.default];
           pkgs = import nixpkgs {
             inherit system overlays;
           };
           rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            rustVersion = rustToolchain.version;
+            packageFun = import ./Cargo.nix;
+          };
         in
         with pkgs;
+        rec
         {
+          packages = {
+            staticify-ip = (rustPkgs.workspace.staticify-ip {});
+            default = packages.staticify-ip;
+         };
           devShells.default = mkShell {
             buildInputs = [
               rustToolchain
               openssl
               pkg-config
               cocogitto
+              cargo2nix.packages.${system}.cargo2nix
             ];
           };
         }
